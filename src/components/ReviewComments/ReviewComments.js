adc32d9ed893ed458ad960dea2473fcd6d28857e
Bode Falade <bode.falade@outlook.com>
2019-01-28 00:18:43 -0600

    Submit buttong becmoes inactive if feedback in not complete.

--- src/components/ReviewComments/ReviewComments.js
+++ src/components/ReviewComments/ReviewComments.js
@@ -7,9 +7,59 @@ import { Button } from '@material-ui/core';
 
 
 class ReviewComments extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            complete: false
+        }
+    }
+
+    handleButton = () => {
+        this.setState({
+            complete: !this.state.complete,
+        })
+    }
+
+    handleClick = () => {
+        const feedback = {
+            feelings: this.props.reduxStore.feedbackReducer.feeling,
+            understanding: this.props.reduxStore.feedbackReducer.understanding,
+            support: this.props.reduxStore.feedbackReducer.support,
+            comments: this.props.reduxStore.feedbackReducer.comments
+        }
+        console.log('submit clicked');
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: feedback
+        }).then((response) => {
+
+        }).catch((error) => {
+            alert('Something Went Wrong in POST! ', error);
+        });
+        
+    }
 
     render(){
+
+            //conditional render for the submit button
+            let handleButton = '';
+            
+            if (this.props.reduxStore.feedbackReducer.feeling !== '' && 
+                this.props.reduxStore.feedbackReducer.understanding !== '' &&
+                this.props.reduxStore.feedbackReducer.support !== '' &&
+                this.props.reduxStore.feedbackReducer.comments !== '')
+                {
+                handleButton = <Button variant="raised" onClick={this.handleClick}>SUBMIT</Button>
+                }
+                else{
+                handleButton = <Button disabled variant="raised" onClick={this.handleClick}>INCOMPLETE</Button>
+                alert('Complete Feedback');
+                }
+
         return(
+
             <div>
                 <h1>Review Your Feedback</h1>
             
@@ -18,7 +68,8 @@ class ReviewComments extends Component{
                 <p>Understanding: {this.props.reduxStore.feedbackReducer.understanding}</p>
                 <p>Support: {this.props.reduxStore.feedbackReducer.support}</p>
                 <p>Comments: {this.props.reduxStore.feedbackReducer.comments}</p>
-
+                {handleButton}
+                {/* <Button variant="raised" onClick={this.handleClick}>SUBMIT</Button> */}
             </div>
         )
     }
