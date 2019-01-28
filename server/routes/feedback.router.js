71d7100e6dc35d71a4a3314379487abb49f705d7
Bode Falade <bode.falade@outlook.com>
2019-01-28 09:49:27 -0600

    README updated.

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,16 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
-const feedback = [];
 
 // GET feedback and show on the Review Feedback page
-router.post('/', (req,res) => {
-    pool.query('SELECT * FROM "feedback";').then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log('Error in GET /review-comments', error);
+router.post('/', async (req,res) => {
+    const feedback = req.body;
+    const queryText = (`INSERT INTO "feedback"("feeling", "understanding",
+                                                "support", "comments")
+                        VALUES ($1, $2, $3, $4);`);
+    
+    pool.query(queryText, [feedback.feelings, feedback.understanding,
+                            feedback.support, feedback.comments]).
+                            then((result) => {
+                                 res.send(201);
+                            }).catch((error) => {
+        console.log('Error in POST /review-comments', error);
         res.sendStatus(500);
-    });
+    })
 });
 
 module.exports = router;
