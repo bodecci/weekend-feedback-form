1252d495e1e647d8c6741777de1b178a7234093e
Bode Falade <bode.falade@outlook.com>
2019-01-27 22:49:23 -0600

    all feedback values and comment, properly displayed in review feedback page.

--- src/components/ReviewComments/ReviewComments.js
+++ src/components/ReviewComments/ReviewComments.js
@@ -14,10 +14,10 @@ class ReviewComments extends Component{
                 <h1>Review Your Feedback</h1>
                 <br></br>
                 {JSON.stringify(this.props.reduxStore)}
-                <p>Feeling: </p>
-                <p>Understanding: </p>
-                <p>Support: </p>
-                <p>Comments: </p>
+                <p>Feeling: {this.props.reduxStore.feedbackReducer.feeling}</p>
+                <p>Understanding: {this.props.reduxStore.feedbackReducer.understanding}</p>
+                <p>Support: {this.props.reduxStore.feedbackReducer.support}</p>
+                <p>Comments: {this.props.reduxStore.feedbackReducer.comments}</p>
 
             </div>
         )
